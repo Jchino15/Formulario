159e99ee030e2bf065f335f89837a5a04486be63
Josue Alvarado <josue.jdam12@gmail.com>
2023-04-21 22:35:08 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Controllers/estados_ReservaController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PracticaMVC.Models;
+
+namespace PracticaMVC.Controllers
+{
+    public class estados_ReservaController : Controller
+    {
+        private readonly equiposDbContext _context;
+
+        public estados_ReservaController(equiposDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+              return _context.estados_Reserva != null ? 
+                          View(await _context.estados_Reserva.ToListAsync()) :
+                          Problem("Entity set 'equiposDbContext.estados_Reserva'  is null.");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.estados_Reserva == null)
+            {
+                return NotFound();
+            }
+
+            var estados_Reserva = await _context.estados_Reserva
+                .FirstOrDefaultAsync(m => m.estado_res_id == id);
+            if (estados_Reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(estados_Reserva);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("estado_res_id,estado")] estados_Reserva estados_Reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estados_Reserva);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estados_Reserva);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.estados_Reserva == null)
+            {
+                return NotFound();
+            }
+
+            var estados_Reserva = await _context.estados_Reserva.FindAsync(id);
+            if (estados_Reserva == null)
+            {
+                return NotFound();
+            }
+            return View(estados_Reserva);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("estado_res_id,estado")] estados_Reserva estados_Reserva)
+        {
+            if (id != estados_Reserva.estado_res_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estados_Reserva);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!estados_ReservaExists(estados_Reserva.estado_res_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estados_Reserva);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.estados_Reserva == null)
+            {
+                return NotFound();
+            }
+
+            var estados_Reserva = await _context.estados_Reserva
+                .FirstOrDefaultAsync(m => m.estado_res_id == id);
+            if (estados_Reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(estados_Reserva);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.estados_Reserva == null)
+            {
+                return Problem("Entity set 'equiposDbContext.estados_Reserva'  is null.");
+            }
+            var estados_Reserva = await _context.estados_Reserva.FindAsync(id);
+            if (estados_Reserva != null)
+            {
+                _context.estados_Reserva.Remove(estados_Reserva);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool estados_ReservaExists(int id)
+        {
+          return (_context.estados_Reserva?.Any(e => e.estado_res_id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PracticaMVC/Controllers/tipo_EquipoController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PracticaMVC.Models;
+
+namespace PracticaMVC.Controllers
+{
+    public class tipo_EquipoController : Controller
+    {
+        private readonly equiposDbContext _context;
+
+        public tipo_EquipoController(equiposDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+              return _context.tipo_Equipo != null ? 
+                          View(await _context.tipo_Equipo.ToListAsync()) :
+                          Problem("Entity set 'equiposDbContext.tipo_Equipo'  is null.");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+
+            var tipo_Equipo = await _context.tipo_Equipo
+                .FirstOrDefaultAsync(m => m.id_tipo_equipo == id);
+            if (tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipo_Equipo);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id_tipo_equipo,descripcion,estado")] tipo_Equipo tipo_Equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipo_Equipo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipo_Equipo);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+
+            var tipo_Equipo = await _context.tipo_Equipo.FindAsync(id);
+            if (tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+            return View(tipo_Equipo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id_tipo_equipo,descripcion,estado")] tipo_Equipo tipo_Equipo)
+        {
+            if (id != tipo_Equipo.id_tipo_equipo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipo_Equipo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!tipo_EquipoExists(tipo_Equipo.id_tipo_equipo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipo_Equipo);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+
+            var tipo_Equipo = await _context.tipo_Equipo
+                .FirstOrDefaultAsync(m => m.id_tipo_equipo == id);
+            if (tipo_Equipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipo_Equipo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.tipo_Equipo == null)
+            {
+                return Problem("Entity set 'equiposDbContext.tipo_Equipo'  is null.");
+            }
+            var tipo_Equipo = await _context.tipo_Equipo.FindAsync(id);
+            if (tipo_Equipo != null)
+            {
+                _context.tipo_Equipo.Remove(tipo_Equipo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool tipo_EquipoExists(int id)
+        {
+          return (_context.tipo_Equipo?.Any(e => e.id_tipo_equipo == id)).GetValueOrDefault();
+        }
+    }
+}
